bb1ad8c73383d7325e2a349a76dd3d8ec6195238
Philistine233 <xinmeng1224@outlook.com>
2025-02-18 14:48:08 -0700

    Linked app with Firebase project

---
+++ code/app/src/main/java/com/example/canada_geese/LoginActivity.java
@@ -0,0 +1,44 @@
+package com.example.canada_geese;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+    private EditText identifierField, passwordField;
+    private Button loginButton;
+    private AuthManager authManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        identifierField = findViewById(R.id.input_identifier);
+        passwordField = findViewById(R.id.input_password);
+        loginButton = findViewById(R.id.button_login);
+        authManager = new AuthManager();
+
+        loginButton.setOnClickListener(view -> {
+            String identifier = identifierField.getText().toString().trim();
+            String password = passwordField.getText().toString().trim();
+
+            authManager.loginUser(identifier, password, new AuthManager.AuthCallback() {
+                @Override
+                public void onSuccess() {
+                    Toast.makeText(LoginActivity.this, "Login Successful!", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                }
+
+                @Override
+                public void onFailure(String errorMessage) {
+                    Toast.makeText(LoginActivity.this, errorMessage, Toast.LENGTH_SHORT).show();
+                }
+            });
+        });
+    }
+}
